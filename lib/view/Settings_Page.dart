15640a2aad38832b92d554868a525d369fa98706
Dipali Thakare <dipalithakare96@gmail.com>
2021-07-30 20:17:18 +0530

    write data to google sheet

--- lib/view/Settings_Page.dart
+++ lib/view/Settings_Page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_demo_project/model/service-model.dart';
 import 'package:http/http.dart' as http;
@@ -13,13 +15,34 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   List<ServiceModel> services = <ServiceModel>[];
 
+  List electrician = [
+    {
+      "name": "Sagar Jadhav",
+      "phone": "98648758",
+      "charges": "200",
+      "Address": "pune"
+    },
+    {
+      "name": "Sagar Jadhav",
+      "phone": "98648758",
+      "charges": "200",
+      "Address": "pune"
+    },
+    {
+      "name": "Sagar Jadhav",
+      "phone": "98648758",
+      "charges": "200",
+      "Address": "pune"
+    },
+  ];
+
   getFeedbackFromSheet() async {
     var raw = await http.get(Uri.parse(
         "https://script.google.com/macros/s/AKfycbygLlV_ITs8s7_kI9xU4pxs85jVTJhK1xBlJYbEJTtL28GEARLEUHTFCSUjRECeqTjF/exec"));
 
     var jsonService = convert.jsonDecode(raw.body);
 
-    print('this is json Feedback $jsonService');
+    print('this is json service $jsonService');
 
     // feedbacks = jsonFeedback.map((json) => FeedbackModel.fromJson(json));
 
@@ -40,9 +63,34 @@ class _SettingsPageState extends State<SettingsPage> {
     }
   }
 
+  postTest() async {
+    //String jsondata = jsonEncode(values);
+
+    //print("helllo $jsondata ");
+    //Map<String, dynamic> args = {"comments": electrician};
+    //  var body = json.encode(electrician);
+
+    var values = {
+      "name": "Sagar Jadhav",
+      "phone": "98648758",
+      "charges": "200",
+      "Address": "pune",
+    };
+
+    var url = Uri.parse(
+        'https://script.google.com/macros/s/AKfycbyhMbjWIL86FxxaXwAK1OlGbEkLDFn6JBVa5waTmnlC02rP5CtGSP38zScQ2j7QgLBA/exec');
+    var response = await http.post(url, body: values);
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    //  print(await http.read(url));
+  }
+
   @override
   void initState() {
-    getFeedbackFromSheet();
+    // getFeedbackFromSheet();
+    // postTest();
     super.initState();
   }
 
@@ -50,7 +98,12 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("Settings"),
+        child: ElevatedButton(
+          onPressed: () {
+            postTest();
+          },
+          child: Text("press"),
+        ),
       ),
     );
   }
